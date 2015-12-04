29e0937939f92d2e674d6a50464e215f5d84f990
docheret <docheret@gmail.com>
2015-12-04 12:06:05 +0200

    test1

--- src/main/java/com/fun/Application.java
+++ src/main/java/com/fun/Application.java
@@ -10,5 +10,18 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+       // isPalindrome("Ah, Satan ees Natasha");
     }
+
+    public static boolean isPalindrome(String value) {
+        String s = value.replaceAll("([^A-Za-z0-9])+","").toLowerCase();
+        for (int i = 0; i < s.length()/2; i++)
+        {
+            if (s.charAt(i) != s.charAt(s.length()-i-1)) return false;
+        }
+        return true;
+    }
+
+
 }
